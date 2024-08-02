5b6a7efb1539bb293d92ed6e7e684a4ed149281f
Vitor Ribeiro <167051938+Vitor-Ribe@users.noreply.github.com>
2024-08-02 14:17:11 -0300

    Create Testar com apenas um arquivo de entrada.sh

---
+++ LAB00/Testar com apenas um arquivo de entrada.sh
@@ -0,0 +1,4 @@
+./a.out <1.in> 1.meu //entra com o arquivo "1.in" no codigo "./a.out" e gera uma arquivo de saída "1.meu"
+
+diff 1.meu 1.out //confere se há alguma diferença entre o arquivo de saída gerado no comando anterior e a saída esperada
+
