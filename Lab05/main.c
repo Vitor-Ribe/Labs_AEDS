4cc6c370355c2f87203402b027976f268fb3bbd6
Vitor Ribeiro <167051938+Vitor-Ribe@users.noreply.github.com>
2024-09-01 13:42:23 -0300

    Add files via upload

--- Lab05/main.c
+++ Lab05/main.c
@@ -9,34 +9,36 @@ No* novo_no(int valor){
     novo->esq = NULL;
     return novo;
 }
-/** Insere valor x na árvore de busca binária */
+/* Insere valor x na árvore de busca binária */
 bool insere(Arvore* ap_arv, int x){
-    if(ap_arv == NULL){
-        ap_arv = novo_no(x);
-        return;
+    if(*ap_arv == NULL){
+        (*ap_arv) = novo_no(x);
+        return true;
     }
-    if(x < (*ap_arv)->valor){
-        insere(&(*ap_arv)->esq, x);
-        return;
-    }
-    if(x > (*ap_arv)->valor){
-        insere(&(*ap_arv)->dir, x);
-        return;
-    }
+    if(x < (*ap_arv)->valor)
+        return insere(&(*ap_arv)->esq, x);
+    if(x > (*ap_arv)->valor)
+        return insere(&(*ap_arv)->dir, x);
+    return false;
 }
-/** Remove valor x da árvore de busca binária. Em caso de nó com dois filhos, deve-se trocar de posição com o antecessor do nó removido e o remover então.  */
+No* maiorNo(Arvore* ap_noEsquerdo){
+    No* maior = (*ap_noEsquerdo);
+    while(maior->dir != NULL){
+        maior = maior->dir;
+    }
+    return maior;
+}
+/* Remove valor x da árvore de busca binária. Em caso de nó com dois filhos, deve-se trocar de posição com o antecessor do nó removido e o remover então.  */
 bool remove_(Arvore* ap_arv, int x){
-
-    if(ap_arv == NULL) return false;
-
-
+    if(*ap_arv == NULL) return false;
     if(x == (*ap_arv)->valor){
-        // caso o nó não tenha filhos
+
         if((*ap_arv)->dir == NULL && (*ap_arv)->esq == NULL){
             free((*ap_arv));
+            (*ap_arv) = NULL;
             return true;
         }
-        // caso o nó tenha apenas 1 filho
+
         if((*ap_arv)->dir == NULL){
             No *filho = (*ap_arv);
             (*ap_arv) = (*ap_arv)->esq;
@@ -49,40 +51,52 @@ bool remove_(Arvore* ap_arv, int x){
             free(filho);
             return true;
         }
-        // caso o nó tenha 2 filhos
+
         if((*ap_arv)->dir != NULL && (*ap_arv)->esq != NULL){
-            No* substituto = menorNo(&(*ap_arv)->dir);
+            No* substituto = maiorNo(&(*ap_arv)->esq);
             (*ap_arv)->valor = substituto->valor;
-            return remove_(&(*ap_arv)->dir, substituto->valor);
+            return remove_(&(*ap_arv)->esq, substituto->valor);
         }
     }
 
-    // movimentação
     if(x < (*ap_arv)->valor)
         return remove_(&(*ap_arv)->esq, x);
     if(x > (*ap_arv)->valor)
         return remove_(&(*ap_arv)->dir, x);
+
+    return false;
 }
-No* menorNo(No* ap_noDireito){
-    No* atual = ap_noDireito;
-    while(atual->esq != NULL){
-        atual = atual->esq;
-    }
-    return atual;
-}
-/** Busca valor x na árvore de busca binária */
+/* Busca valor x na árvore de busca binária */
 bool busca(Arvore arv, int x){
     if(arv == NULL) return false;
     if(arv->valor == x) return true;
-    if(x < arv->valor)
-        return busca(arv->esq, x);
-    if(x > arv->valor)
-        return busca(arv->dir, x);
+    if(x < arv->valor) return busca(arv->esq, x);
+    if(x > arv->valor) return busca(arv->dir, x);
+    return false;
 }
-/** Imprime a árvore de busca binária */
+/*  Imprime a árvore de busca binária */
+void auxImpressao(Arvore arv){
+    if(arv){
+        printf("[%d:e=", arv->valor);
+        // Imprime filho esquerdo
+        if(arv->esq)
+            auxImpressao(arv->esq);
+        else
+            printf(".");
+
+        printf(",d=");
+        // Imprime filho direito
+        if(arv->dir)
+            auxImpressao(arv->dir);
+        else
+            printf(".");
+        printf("]");
+    }
+}
 void imprime(Arvore arv){
-
-
-
-
+    if(arv){
+        auxImpressao(arv);
+        printf("\n");
+    }else
+        printf(".\n");
 }
