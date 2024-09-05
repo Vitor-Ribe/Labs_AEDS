907268ccb48845f1b5aae5c2e55395e9a2289b31
Vitor Ribeiro <167051938+Vitor-Ribe@users.noreply.github.com>
2024-09-05 10:09:40 -0300

    Add files via upload

--- LAB04/mochila.c
+++ LAB04/mochila.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
 void preencheVetor(int n, int vet[2][n]){
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++)
         scanf("%d %d", &vet[0][i], &vet[1][i]);
-    }
 }
 
 void subconj(int n, int M, int elem[][n]){
-    int limit_mask = (1<<n); // conjunto
-    int melhorValor = 0, melhorPeso = 0;
+    int limit_mask = (1<<n); // conjunto potencia
+    int melhorValor = 0;
 
     for(int mask=0; mask<limit_mask; mask++){   // percorre todos os subconjuntos possiveis
         int totalPeso = 0, totalValor = 0;
 
-        for(int i=0; i<n; i++){ //  percorre todos os elementos do subconjunto
+        for(int i=0; i<n; i++) //  percorre todos os elementos do subconjunto
             if(mask & (1<<i)){  //  somente os bits ativos
                 totalPeso += elem[0][i];
                 totalValor += elem[1][i];
             }
-        }
         if(totalPeso <= M && totalValor > melhorValor){ //  compara se o valor e o peso do subconjunto é o maior do conjunto
             melhorValor = totalValor;
-            melhorPeso = totalPeso;
         }
     }
     printf("%d\n", melhorValor);
@@ -33,13 +30,11 @@ int main(){
     scanf("%d", &n);
     if(n <= 0 || n > 20)
         return -1;
-
     scanf("%d", &M);
     if(M <= 0 || M > 1000000)
         return -1;
 
     int elemento[2][n];
-
     preencheVetor(n, elemento);
     subconj(n, M, elemento);
     return 0;
